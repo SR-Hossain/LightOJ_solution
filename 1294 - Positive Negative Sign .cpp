d363748e68c20c60b7e48ae21453828671df561d
SR-Hossain <65627729+SR-Hossain@users.noreply.github.com>
2020-05-26 03:17:35 +0600

    A simple rule of mathematics
    result = (n*m)/2
    in long long int

---
+++ 1294 - Positive Negative Sign .cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main()
+{
+    int t;cin>>t;
+    for(int ff=1;ff<=t;ff++){
+        ll n,m;cin>>n>>m;
+        cout<<"Case "<<ff<<": "<<(ll)n*m/2<<'\n';
+    }
+}
